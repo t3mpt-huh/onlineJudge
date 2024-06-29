07966ca73d27dea703d4a3393b3c6a346333391e
t3mpt-huh <143261576+t3mpt-huh@users.noreply.github.com>
2024-06-29 17:49:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ const User = require('./models/Users.js');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
 dotenv.config();
 
 //middldewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 //connect to database
 DBConnection();
@@ -53,6 +55,52 @@ app.post("/register",async (req,res)=>{
         console.error(error);
     }
 });
+
+app.post('/login',async (req,res)=>{
+    try {
+        //get all details from frontend
+        const {email,password} = req.body;
+
+        //validating user
+        if(!(email && password)){
+            return res.status(400).send('Fill all details');
+        }
+
+        //find user in database
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).send('No such user exists');
+        }
+
+        //match password
+        const match = await bcrypt.compare(password,user.password);
+        if(!match){
+            return res.status(400).send('Wrong password');
+        }
+        //password matches hence we reach here -> now use jwt
+        const token = jwt.sign({id:user._id},process.env.SECRET_KEY,{
+            expiresIn:'1h'
+        });
+        user.token = token;
+        user.password = undefined;
+
+        //section for cookies -> sending token in user cookie
+        const options = {
+            expires : new Date(Date.now() + 3*24*60*60*60*1000),
+            httpOnly : true
+        };
+        res.status(200).cookie("token", token, options).json({
+            success : true,
+            token,
+            user
+        });
+        
+    } catch (error) {
+        //console.log('galat hai aap');
+        console.log(error.message);
+    }
+})
+
 app.listen(8000,()=>{
     console.log("Server running on port: 8000");
 });
