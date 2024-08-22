35e713a6500b6f58ae1fa223eb708c066281f881
t3mpt-huh <anshbansal270@gmail.com>
2024-08-22 22:37:07 +0530

    Updated server.js for CORS Opstions

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,21 @@ const submissionsRoute = require("./router/submissions-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
-app.use(cors());
+// Define CORS options
+const corsOptions = {
+  origin: [
+    'https://lethimcode.pro',
+    'https://www.lethimcode.pro',
+    'http://localhost:5173',
+    'http://localhost:4890',
+    'https://online-judge-t3-ansh-bansals-projects-15f560f5.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
